e9e27d8a8fd2433611ebb340223081aba777bb52
jaysonrighthere <jayson7498@gmail.com>
2021-06-28 10:02:20 +0700

    Mock up authen, processing the auth

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+
+
+export const auth = firebase.initializedApp({
+  apiKey: "AIzaSyCavX5cTC0yN4bXCJPdYJoVWQBnbjNbPZc",
+  authDomain: "unichat-f5fa5.firebaseapp.com",
+  projectId: "unichat-f5fa5",
+  storageBucket: "unichat-f5fa5.appspot.com",
+  messagingSenderId: "198876671430",
+  appId: "1:198876671430:web:21e56f65c7e296c66f56c7",
+  measurementId: "G-PC3XRG9BWC"
+}).auth();
+
+
